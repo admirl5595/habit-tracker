1018c6a3f6b1edf0fbe2f34f9e64a1f10bfe1e76
Jesper Ibsen <jesperi@uia.no>
2022-03-29 18:55:04 +0200

    Show who's signed in

--- screens/Settings.js
+++ screens/Settings.js
@@ -3,21 +3,23 @@ import {
   View,
   StyleSheet,
   ScrollView,
+  Text,
 } from "react-native";
 import HabitsContext from "../config/HabitsContext";
-
 import DropDownItems from "../components/settings/DropDownItems";
 import LogoutBtn from "../components/auth/LogoutBtn";
+import { auth } from "./../firebase-config";
 
 export default function Settings({navigation}) {
 
   const { habits } = useContext(HabitsContext);
 
+  // Show a settings interface for user
   return (
     <View style={styles.container}>
       <ScrollView style={{ alignSelf: "stretch" }}>
-        <LogoutBtn/>
-
+      <Text style={{textAlign: 'left', fontSize: 18}}> Signed in as:  {auth.currentUser.email}</Text>
+        <LogoutBtn/> 
         <View>
           <DropDownItems
             listName="My Habits"
